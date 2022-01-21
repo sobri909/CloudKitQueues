9b3db31be56b5acc493529740a7ee880b2774b3b
Matt <matt@bigpaua.com>
2022-01-21 18:58:54 +0700

    let's see if this works for SwiftPM

---
+++ Package.swift
@@ -0,0 +1,14 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "CloudKitQueues",
+    products: [
+        .library(name: "CloudKitQueues", targets: ["CloudKitQueues"])
+    ],
+    targets: [
+        .target(name: "CloudKitQueues", path: "Source")
+    ]
+)
